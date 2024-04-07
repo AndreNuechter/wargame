c302d94f94953d1fca1c336adb7d2d6c63ba6a78
nuechter <andre.nuechter@neofonie.de>
2024-04-07 10:39:01 +0200

    Fix coordinates, impl neighbor calculation and some refactoring

--- src/js-modules/dom-creations.js
+++ src/js-modules/dom-creations.js
@@ -1,17 +1,15 @@
-export const outerHexTmpl = (() => {
-    const use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
-    use.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#outer-hex');
-    use.classList.add('outer-cell');
-    return use;
-})();
-export const innerHexTmpl = (() => {
-    const use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
-    use.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#inner-hex');
-    use.classList.add('inner-cell');
-    return use;
-})();
 export const cellGroupTmpl = (() => {
-    const group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-    group.classList.add('cell');
-    return group;
+    const outerHex = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+    const innerHex = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+    const cellWrapper = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+
+    innerHex.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#inner-hex');
+    outerHex.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#outer-hex');
+    innerHex.classList.add('inner-cell');
+    outerHex.classList.add('outer-cell');
+    cellWrapper.classList.add('cell');
+
+    cellWrapper.append(outerHex, innerHex);
+
+    return cellWrapper;
 })();
--- src/main.js
+++ src/main.js
@@ -1,73 +1,174 @@
 import './js-modules/service-worker-init.js';
 import { board } from './js-modules/dom-selections.js';
-import { outerHexTmpl, innerHexTmpl, cellGroupTmpl } from './js-modules/dom-creations.js';
+import { cellGroupTmpl } from './js-modules/dom-creations.js';
 
-// TODO add bioms and pseudo-randomnly apply them to cells
+// TODO add bioms (colors and icons for now) and (pseudo-)randomnly apply them to cells
 
 clear_board();
-const hex_map = create_hex_cell(7, 4)
+const board_dimensions = { width: 7, height: 8 };
+const hex_map = create_hex_cell(board_dimensions)
     .reduce((hex_cell_map, hex, id) => {
         hex_cell_map.set(render_cell(hex, id), hex);
         return hex_cell_map;
     }, new Map());
-
-// TODO highlight neighbors on click
-board.addEventListener('click', ({ target }) => {
-    const cell_element = target.closest('.cell');
-    console.log(hex_map.get(cell_element), target);
+// precompute neighbors
+[...hex_map.values()].forEach((hex_obj) => {
+    hex_obj.neighbors = get_neighboring_cells(hex_obj, [...hex_map]);
 });
 
-function get_neighboring_cells(cell, cells) {
-    // TODO impl me
+document.getElementById('toogle-coord-system').addEventListener('click', () => {
+    document.body.classList.toggle('use-offset-coords');
+});
+
+// highlight neighbors on click
+board.addEventListener('click', ({ target }) => {
+    const cell_element = target.closest('.cell');
+
+    if (!cell_element) return;
+
+    const previously_selected_cell = document.querySelector('.clicked');
+
+    if (previously_selected_cell) {
+        previously_selected_cell.classList.remove('clicked');
+        document.querySelectorAll('.adjacent-to-clicked').forEach((cell) => cell.classList.remove('adjacent-to-clicked'));
+    }
+
+    cell_element.classList.add('clicked');
+    hex_map.get(cell_element).neighbors.forEach((cell) => cell.classList.add('adjacent-to-clicked'));
+});
+
+function get_neighboring_cells({
+    q, r, s, x, y
+}, cells) {
+    const direction_vectors = [
+        { q: 1, r: 0, s: -1 },
+        { q: 1, r: -1, s: 0 },
+        { q: 0, r: 1, s: -1 },
+        { q: 0, r: -1, s: 1 },
+        { q: -1, r: 0, s: 1 },
+        { q: -1, r: 1, s: 0 }
+    ];
+    const neighbors = direction_vectors.map((vec) =>
+        cells.find(
+            ([, cell]) =>
+                cell.q === q + vec.q &&
+                cell.r === r + vec.r &&
+                cell.s === s + vec.s
+        )
+    ).filter(Boolean);
+
+    // wrap around left edge
+    if (x === 0) {
+        // add the rightmost cell on the same row
+        neighbors.push(
+            cells.find(([, cell]) => y === cell.y && cell.x === board_dimensions.width - 1)
+        );
+
+        // add the, at most two, cells above and below that as well
+        // NOTE: leftmost cells on odd rows only have the one neighbor already added
+        if (is_even(y)) {
+            neighbors.push(
+                ...[
+                    y < board_dimensions.height - 1
+                        ? cells.find(([, cell]) => y + 1 === cell.y && cell.x === board_dimensions.width - 1)
+                        : null,
+                    y > 0
+                        ? cells.find(([, cell]) => y - 1 === cell.y && cell.x === board_dimensions.width - 1)
+                        : null
+                ].filter(Boolean)
+            );
+        }
+    }
+
+    // wrap around right edge
+    if (x === board_dimensions.width - 1) {
+        // add the leftmost cell on the same row
+        neighbors.push(cells.find(([, cell]) => y === cell.y && cell.x === 0));
+
+        // add the, at most two, cells above and below that as well
+        // NOTE: rightmost cells on even rows only have the one neighbor already added
+        if (!is_even(y)) {
+            neighbors.push(
+                ...[
+                    y < board_dimensions.height - 1
+                        ? cells.find(([, cell]) => y + 1 === cell.y && cell.x === 0)
+                        : null,
+                    y > 0
+                        ? cells.find(([, cell]) => y - 1 === cell.y && cell.x === 0)
+                        : null
+                ].filter(Boolean)
+            );
+        }
+    }
+
+    return neighbors.map(([cell_element]) => cell_element);
+}
+
+function is_even(num) {
+    return num % 2 === 0;
 }
 
 function clear_board() {
     board.innerHTML = '';
 }
 
-function render_cell({ cx, cy }, id) {
-    const group = cellGroupTmpl.cloneNode(true);
-    const wrapper = outerHexTmpl.cloneNode(true);
-    const filler = innerHexTmpl.cloneNode(true);
+function render_cell({
+    cx, cy, x, y, q, r, s
+}, id) {
+    const cellWrapper = cellGroupTmpl.cloneNode(true);
 
-    wrapper.setAttribute('transform', `translate(${cx}, ${cy})`);
-    filler.setAttribute('transform', `translate(${cx}, ${cy})`);
+    cellWrapper.setAttribute('transform', `translate(${cx}, ${cy})`);
+    cellWrapper.dataset.id = id;
 
-    group.dataset.id = id;
-    group.append(wrapper, filler);
+    const text_group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    const q_coord = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    const r_coord = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    const s_coord = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    const offset_ccords = document.createElementNS('http://www.w3.org/2000/svg', 'text');
 
-    board.append(group);
+    // render coords
+    q_coord.textContent = `${q}`;
+    r_coord.textContent = `${r}`;
+    s_coord.textContent = `${s}`;
+    offset_ccords.textContent = `${x}, ${y}`;
+    q_coord.setAttribute('transform', `translate(${1.5}, ${2})`);
+    r_coord.setAttribute('transform', `translate(${4.5}, ${3.5})`);
+    s_coord.setAttribute('transform', `translate(${2}, ${5})`);
+    offset_ccords.setAttribute('transform', `translate(${1.5}, ${3.5})`);
+    q_coord.classList.add('cube-coord');
+    r_coord.classList.add('cube-coord');
+    s_coord.classList.add('cube-coord');
+    offset_ccords.classList.add('offset-coord');
+    text_group.append(q_coord, r_coord, s_coord, offset_ccords);
+    cellWrapper.append(text_group);
 
-    return group;
+    board.append(cellWrapper);
+
+    return cellWrapper;
 }
 
-function create_hex_cell(height, width) {
-    const heax_cells = [];
+function create_hex_cell({ height, width }) {
+    const hex_cells = [];
 
-    for (let y = 0; y < height; y += 1) {
-        // the horizontal distance between adjacent hexagon centers is horiz = width = sqrt(3) * size.
-        // The vertical distance is vert == 3 / 4 * height == 3 / 2 * size.
-        // NOTE: as the vertical edges are longer, we dont have regular hexagons
-        const odd_row = Number(y % 2 > 0);
-        // if height is odd we skip the bottom-left cell otherwise the bottom-right
-        const bottom_cell_to_skip = height % 2 > 0
-            ? 0
-            : width - 1;
+    for (let row = 0; row < height; row += 1) {
+        const odd_row = Number(!is_even(row));
 
-        for (let x = 0; x < width; x += 1) {
-            // skip outlier cells to consistently have 6 neighbors
-            if (x === 0 && y === 0) continue;
-            if (x === bottom_cell_to_skip && y === (height - 1)) continue;
+        for (let col = 0; col < width; col += 1) {
+            const q = col - (row - (row & 1)) / 2;
 
-            heax_cells.push({
-                cx: (x * 6) + (odd_row * 3),
-                cy: y * 4.5,
-                x,
-                y,
-                z: -x - y
+            hex_cells.push({
+                cx: (col * 6) + (odd_row * 3),
+                cy: row * 4.5,
+                // offset coords
+                x: col,
+                y: row,
+                // cube coords
+                q,
+                r: row,
+                s: -q - row
             });
         }
     }
 
-    return heax_cells;
+    return hex_cells;
 }
